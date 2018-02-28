a063f9a3c952884e12719168244ed1e8651e300f
vincentmuya <vincentmuya13@gmail.com>
2018-02-28 16:44:27 +0300

    created Newsletter class

--- tribune/news/views.py
+++ tribune/news/views.py
@@ -1,14 +1,22 @@
 from django.http import HttpResponse
 import datetime as dt
 from django.shortcuts import render,redirect
+from .models import Article
+from .forms import NewsLetterForm
 
 # Create your views here.
-def welcome(request):
-    return render(request,'welcome.html')
-
-def news_of_day(request):
+def news_today(request):
     date = dt.date.today()
-    return render(request, 'all-news/today-news.html',{"date":date,})
+    news = Article.todays_news()
+    
+
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            print('Valid')
+    else:
+        form = NewsLetterForm()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})
 
 
 def past_days_news(request,past_date):
@@ -24,4 +32,26 @@ def past_days_news(request,past_date):
 
         if date == dt.date.today():
             return redirect(news_of_day)
-        return render(request, 'all-news/past-news.html', {"date":date})
+
+        news = Article.day_news(date)
+        return render(request, 'all-news/past-news.html', {"date":date, "news":news})
+
+def search_results(request):
+
+    if 'article' in request.GET and request.GET["article"]:
+        search_term = request.GET.get("article")
+        searched_articles = Article.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-news/search.html',{"message":message})
+
+def article(request,article_id):
+    try:
+        article = Article.objects.get(id = article_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"all-news/article.html", {"article":article})
